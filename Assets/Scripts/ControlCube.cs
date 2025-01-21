302103f3034a38a65b9ef511eb7039b84e207a3e
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2025-01-21 13:54:22 +0000

    move cube backwards and forwards

--- Assets/Scripts/ControlCube.cs
+++ Assets/Scripts/ControlCube.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class ControlCube : MonoBehaviour
 {
+    float cubeSpeed = 0.002f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * 0.003f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * cubeSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * cubeSpeed;
+        }
     }
 }
